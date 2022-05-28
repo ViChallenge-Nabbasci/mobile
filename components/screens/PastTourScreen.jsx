e7e45f7e939a4da72b0ee5aff78c9f3df8e853df
Simone Rega <simone.rega2@studio.unibo.it>
2022-05-28 03:11:31 +0200

    Create PastTourScreen.jsx

---
+++ components/screens/PastTourScreen.jsx
@@ -0,0 +1,10 @@
+import { Layout, Text, Avatar,Button,IconRegistry, Icon,Card,Toggle ,Divider} from '@ui-kitten/components';
+import React from "react";
+import { StyleSheet,View, SafeAreaView, Image, ScrollView } from 'react-native';
+import { Row, Col, Grid } from "react-native-easy-grid";
+
+export const PastTourScreen = () =>{
+    return(
+        null
+    )
+};
